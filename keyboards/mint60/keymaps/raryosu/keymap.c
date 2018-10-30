1b002a0931da227842e801121e2b139899d2db95
raryosu <me@raryosu.info>
2018-10-30 17:50:54 +0900

    Update Keymap

--- keyboards/mint60/keymaps/raryosu/keymap.c
+++ keyboards/mint60/keymaps/raryosu/keymap.c
@@ -46,7 +46,7 @@ enum custom_keycodes {
  * |------------------------------------------.     `------------------------------------------------|
  * | LShift     |  Z  |  X  |  C  |  V  |  B  |       |  N  |  M  |  ,  |  .  |  /  | Sft| Up  | Fn  |
  * |------------------------------------------|     ,------------------------------------------------|
- * | ESC  |  Ctrl  |  Alt  |  Cmd  |  Space   |     |   BS   |  Enter |   GRV       | Lft| Dwn | Rgt |
+ * | ESC  |  Ctrl  |  Alt  |  Cmd  |  Space   |     |   BS   |  Enter |   Cmd       | Lft| Dwn | Rgt |
  * `------------------------------------------'     `------------------------------------------------'
  */
 /* Keymap 1: Symbol layer
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  \
     KC_LCTL,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,        KC_ENT,   \
     KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,   KC_UP,  MO(1),   \
-    KC_ESC,    KC_LCTL,    KC_LALT,    KC_LGUI,    KC_SPC,        KC_BSPC, KC_ENT,  LALT(KC_GRV),               KC_LEFT,KC_DOWN,KC_RGHT \
+    KC_ESC,    KC_LCTL,    KC_LGUI,      KC_LALT,    KC_SPC,        KC_BSPC, KC_ENT,  KC_RGUI,               KC_LEFT,KC_DOWN,KC_RGHT \
   ),
   [1] = LAYOUT( \
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_DEL, \
