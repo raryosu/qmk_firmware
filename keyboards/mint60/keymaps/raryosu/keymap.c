1a21e99be2471ebefc31efe9d1769dd07c412c8d
raryosu <me@raryosu.info>
2018-11-17 10:39:53 +0900

    Layerを追加

--- keyboards/mint60/keymaps/raryosu/keymap.c
+++ keyboards/mint60/keymaps/raryosu/keymap.c
@@ -63,20 +63,40 @@ enum custom_keycodes {
  * |      |        |       |       |   BSPC   |     |        |        |             |Home| PDwn| End |
  * `------------------------------------------'     `------------------------------------------------'
  */
-
+/* Keymap 3: Programming layer
+ *
+ * ,------------------------------------.      ,-----------------------------------------------------.
+ * | ESC  | F1  | F2  | F3  | F4  | F5  |      | F6  | F7  |  F8 | F9  | F10 | F11 | F12 | Del       |
+ * |--------------------------------------.    `-----------------------------------------------------|
+ * | LED_TOG|     |     |     |     |  [  |       |  ]  |     |     |     |     |     |     |        |
+ * |----------------------------------------.     `--------------------------------------------------|
+ * |          |     |     |     |     |  \  |       | Lft | Dwn | Up | Rgt |     |     |            |
+ * |------------------------------------------.     `------------------------------------------------|
+ * |            |     |     |     |     | BPSC|       |  ;  |     |     |     |     |    | PUp | Fn  |
+ * |------------------------------------------|     ,------------------------------------------------|
+ * |      |        |       |       |   BSPC   |     |        |        |             |Home| PDwn| End |
+ * `------------------------------------------'     `------------------------------------------------'
+ */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT( \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSPC,  \
     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  \
     KC_LCTL,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,        KC_ENT,   \
     KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,   KC_UP,  MO(1),   \
-    KC_ESC,    KC_LCTL,    KC_LGUI,      KC_LALT,    KC_SPC,        KC_BSPC, KC_ENT,  KC_RGUI,               KC_LEFT,KC_DOWN,KC_RGHT \
+    KC_ESC,    KC_LALT,    KC_LGUI,     MO(2) ,    KC_SPC,        KC_BSPC, KC_ENT,  KC_RALT,               KC_LEFT,KC_DOWN,KC_RGHT \
   ),
   [1] = LAYOUT( \
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_DEL, \
     RGB_TOG,   RGBRST,  RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
     XXXXXXX,     RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, \
     _______,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, KC_PGUP, _______, \
+    XXXXXXX,   _______,    _______,    _______,    XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX,                   KC_HOME, KC_PGDN, KC_END \
+  ),
+  [2] = LAYOUT( \
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_DEL, \
+    RGB_TOG,   RGBRST,  RGB_HUI, RGB_SAI, RGB_VAI, KC_LBRC,   KC_RBRC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+    XXXXXXX,     RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, KC_BSLS,   KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, XXXXXXX, XXXXXXX,        XXXXXXX, \
+    _______,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   KC_SCLN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, KC_PGUP, _______, \
     XXXXXXX,   _______,    _______,    _______,    XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX,                   KC_HOME, KC_PGDN, KC_END \
   )
 };
